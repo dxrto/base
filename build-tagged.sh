5f6599c893456db56349b5b0c0ce768d13089a8c
eater <=@eater.me>
2018-12-20 23:05:09 +0100

    Add glibc container

--- build-tagged.sh
+++ build-tagged.sh
@@ -5,4 +5,4 @@ for toolbox in "none" "toybox" "busybox" "default"; do
     make IMAGE="${IMAGE}:${toolbox}" TOOLBOX="${toolbox}";
 done
 docker tag "${IMAGE}:toybox" "${IMAGE}:latest";
-
+make IMAGE="${IMAGE}:glibc" TOOLBOX="toybox" ARCH="x86_64"
